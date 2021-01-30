ded7a49894398bee5366836ddecb01739e4aaca7
leodelgadodev <leodelgado.dev@gmail.com>
2021-01-30 12:06:15 -0300

    global styles to ios mode

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -12,7 +12,12 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot({
+      mode: 'ios'
+    }), 
+    AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
